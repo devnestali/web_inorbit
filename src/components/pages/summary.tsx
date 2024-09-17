d61e87c6affc8fc497e124032d8a7e2539db2a6c
Victor Henrique de Camargo Nestali <victornestali@gmail.com>
2024-09-17 17:23:13 +0200

    Making data connection of goals completed in a week

--- src/components/pages/summary.tsx
+++ src/components/pages/summary.tsx
@@ -84,49 +84,36 @@ export function Summary() {
       <div className="flex flex-col gap-6">
         <h2 className="text-xl font-medium">Sua semana</h2>
 
-        <div className="flex flex-col gap-4">
-          <h3 className="font-medium">
-            Domingo{' '}
-            <span className="text-zinc-400 text-xs">(10 de Agosto)</span>
-          </h3>
+        {Object.entries(data.goalsPerDay).map(([date, goals]) => {
+          const weekDay = dayjs(date).format('dddd')
+          const formattedDate = dayjs(date).format('D[ de ]MMMM')
 
-          <ul className="flex flex-col gap-3">
-            <li className="flex items-center gap-2">
-              <CheckCircle2 className="size-4 text-pink-500" />
-              <span className="text-sm text-zinc-400">
-                Você completou "
-                <span className="text-zinc-100">Acordar cedo</span>" às{' '}
-                <span className="text-zinc-100">08:13h</span>
-              </span>
-            </li>
+          return (
+            <div key={date} className="flex flex-col gap-4">
+              <h3 className="font-medium ">
+                <span className="capitalize">{weekDay}</span>{' '}
+                <span className="text-zinc-400 text-xs">({formattedDate})</span>
+              </h3>
 
-            <li className="flex items-center gap-2">
-              <CheckCircle2 className="size-4 text-pink-500" />
-              <span className="text-sm text-zinc-400">
-                Você completou "
-                <span className="text-zinc-100">Acordar cedo</span>" às{' '}
-                <span className="text-zinc-100">08:13h</span>
-              </span>
-            </li>
-          </ul>
-        </div>
+              <ul className="flex flex-col gap-3">
+                {goals.map(goal => {
+                  const time = dayjs(goal.completedAt).format('HH:mm')
 
-        <div className="flex flex-col gap-4">
-          <h3 className="font-medium">
-            Segunda-feira{' '}
-            <span className="text-zinc-400 text-xs">(11 de Agosto)</span>
-          </h3>
-
-          <ul className="flex flex-col gap-3">
-            <li className="flex items-center gap-2">
-              <CheckCircle2 className="size-4 text-pink-500" />
-              <span className="text-sm text-zinc-400">
-                Você completou "<span className="text-zinc-100">Meditar</span>"
-                às <span className="text-zinc-100">11:13h</span>
-              </span>
-            </li>
-          </ul>
-        </div>
+                  return (
+                    <li key={goal.id} className="flex items-center gap-2">
+                      <CheckCircle2 className="size-4 text-pink-500" />
+                      <span className="text-sm text-zinc-400">
+                        Você completou "
+                        <span className="text-zinc-100">{goal.title}</span>" às{' '}
+                        <span className="text-zinc-100">{time}h</span>
+                      </span>
+                    </li>
+                  )
+                })}
+              </ul>
+            </div>
+          )
+        })}
       </div>
     </div>
   )
