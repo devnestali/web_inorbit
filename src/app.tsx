a30d9f32a0a445d5612d6fd523e493703e7e398c
Victor Henrique de Camargo Nestali <victornestali@gmail.com>
2024-09-17 11:52:01 +0200

    Creating logic to exhibition between pages summary and empty-goals using tanstack lib

--- src/app.tsx
+++ src/app.tsx
@@ -1,13 +1,19 @@
 import { Dialog } from './components/dialog'
 import { CreateGoal } from './components/pages/create-goal'
 import { Summary } from './components/pages/summary'
-//import { EmptyGoals } from './components/pages/empty-goals'
+import { EmptyGoals } from './components/pages/empty-goals'
+import { useQuery } from '@tanstack/react-query'
+import { getSummary } from './http/get-summary'
 
 export function App() {
+  const { data } = useQuery({
+    queryKey: ['summary'],
+    queryFn: getSummary,
+  })
+
   return (
     <Dialog>
-      {/* <EmptyGoals /> */}
-      <Summary />
+      {data?.total && data.total > 0 ? <Summary /> : <EmptyGoals />}
 
       <CreateGoal />
     </Dialog>
